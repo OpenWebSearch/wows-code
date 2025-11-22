8755026b142f21f680c4ccce391fda8ac1993549
Maik Fröbe <maik.froebe@uni-jena.de>
2025-11-22 13:34:38 +0100

    improve pyterrier baseline

--- ecir26/pyterrier-baseline/README.md
+++ ecir26/pyterrier-baseline/README.md
@@ -1,6 +1,46 @@
 # PyTerrier Baseline
 
 
+Get an overview of all possibilities to execute this baseline via:
+
+```
+./retrieve.py --help
+```
+
+This should yield an output like:
+
+```
+Usage: retrieve.py [OPTIONS]
+
+Options:
+  --dataset [radboud-validation-20251114-training|spot-check-20251122-training]
+                                  The dataset.  [required]
+  --output PATH                   The output directory.  [required]
+  --retrieval-model TEXT          The retrieval model.
+  --index [default|title|description]
+                                  The text field of the index on which to
+                                  retrieve.
+  --help                          Show this message and exit.
+```
+
+## Run All Variants
+
+We run retrieval via:
+
+```
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model BM25 --index default
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model PL2 --index default
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model DirichletLM --index default
+
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model BM25 --index title
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model PL2 --index title
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model DirichletLM --index title
+
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model BM25 --index description
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model PL2 --index description
+./retrieve.py --dataset DATASET-ID --output runs --retrieval-model DirichletLM --index description
+```
+
 ## Indexing
 
 Submit the indexing as code submission to tira
--- ecir26/pyterrier-baseline/retrieve.py
+++ ecir26/pyterrier-baseline/retrieve.py
@@ -4,31 +4,27 @@ import pyterrier as pt
 from pathlib import Path
 from tira.third_party_integrations import ensure_pyterrier_is_loaded
 from tirex_tracker import tracking, ExportFormat
-from tira.rest_api_client import Client
-import pandas as pd
-import json
-from tqdm import tqdm
 
 
 @click.command()
 @click.option("--dataset", type=click.Choice(["radboud-validation-20251114-training", "spot-check-20251122-training"]), required=True, help="The dataset.")
 @click.option("--output", type=Path, required=True, help="The output directory.")
-@click.option("--text-field", type=click.Choice(["default_text", "title", "description"]), required=False, default="title", help="The text field to index.")
-def main(dataset, text_field, output):
-    pt.Artifact.from_url(f"tira:{dataset}/ows/pyterrier-index-default")
+@click.option("--retrieval-model", type=str, default="BM25", required=False, help="The retrieval model.")
+@click.option("--index", type=click.Choice(["default", "title", "description"]), required=False, default="default", help="The text field of the index on which to retrieve.")
+def main(dataset, index, retrieval_model, output):
+    ensure_pyterrier_is_loaded()
+    pt_index = pt.Artifact.from_url(f"tira:{dataset}/ows/pyterrier-index-{index}")
+    topics = pt.datasets.get_dataset(f"irds:ir-lab-wise-2025/{dataset}").get_topics("title")
+    retriever = pt.terrier.Retriever(pt_index, wmodel=retrieval_model)
 
-    tag = f"pyterrier-{text_field}"
-    description = f"This is a PyTerrier index that indexes the field {text_field} with default settings."
-    corpus_file = Client().download_dataset(None, dataset) / "corpus.jsonl.gz"
-    lines = pd.read_json(corpus_file, lines=True)
+    tag = f"pyterrier-{retrieval_model}-on-{index}"
+    target_dir = output / dataset / tag
+    description = "This is a PyTerrier retriever using the retrieval model {retriever} retrieving on the {index} text representation of the documents. Everything is set to the defaults."
 
-    docs = []
-    for _, l in tqdm(lines.iterrows(), "load docs"):
-        docs.append({"docno": l["doc_id"], "text": l[text_field]})
+    with tracking(export_file_path=target_dir / "retrieval-metadata.yml", export_format=ExportFormat.IR_METADATA, system_description=description, system_name=tag):
+        run = retriever(topics)
 
-    with tracking(export_file_path=output / "index-metadata.yml", export_format=ExportFormat.IR_METADATA, system_description=description, system_name=tag):
-        pt.IterDictIndexer(str(output.absolute()), meta={'docno' : 100}).index(docs)
-
+    pt.io.write_results(run, target_dir / "run.txt.gz")
 
 if __name__ == '__main__':
     main()
