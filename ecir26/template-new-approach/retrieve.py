46a765510d7d7ab09e77c74404726305ee7db208
Maik Fröbe <maik.froebe@uni-jena.de>
2025-11-23 18:07:12 +0100

    modify template for new approach

--- ecir26/template-new-approach/retrieve.py
+++ ecir26/template-new-approach/retrieve.py
@@ -30,29 +30,37 @@ def get_index(dataset, field, output_path):
             pt.IterDictIndexer(str(index_dir.absolute()), meta={'docno' : 100}, verbose=True).index(docs)
 
     return pt.IndexFactory.of(str(index_dir.absolute()))
-    
+
+
+def run_retrieval(output, index, dataset, retrieval_model, text_field_to_retrieve):
+    tag = f"pyterrier-{retrieval_model}-on-{text_field_to_retrieve}"
+    target_dir = output / "runs" / dataset / tag
+    target_file = target_dir / "run.txt.gz"
+
+    if target_file.exists():
+        return
+
+    topics = pt.datasets.get_dataset(f"irds:ir-lab-wise-2025/{dataset}").get_topics("title")
+    retriever = pt.terrier.Retriever(index, wmodel=retrieval_model)
+
+    description = f"This is a PyTerrier retriever using the retrieval model {retriever} retrieving on the {text_field_to_retrieve} text representation of the documents. Everything is set to the defaults."
+
+    with tracking(export_file_path=target_dir / "retrieval-metadata.yml", export_format=ExportFormat.IR_METADATA, system_description=description, system_name=tag):
+        run = retriever(topics)
+
+    pt.io.write_results(run, target_file)
 
 @click.command()
 @click.option("--dataset", type=click.Choice(["radboud-validation-20251114-training", "spot-check-20251122-training"]), required=True, help="The dataset.")
 @click.option("--output", type=Path, required=False, default=Path("output"), help="The output directory.")
-@click.option("--retrieval-model", type=str, default="BM25", required=False, help="The retrieval model.")
+@click.option("--retrieval-model", type=str, default="BM25", required=False, help="The retrieval model (e.g., BM25, PL2, DirichletLM).")
 @click.option("--text-field-to-retrieve", type=click.Choice(["default_text", "title", "description"]), required=False, default="default_text", help="The text field of the documents on which to retrieve.")
 def main(dataset, text_field_to_retrieve, retrieval_model, output):
     ensure_pyterrier_is_loaded(is_offline=False)
-    topics = pt.datasets.get_dataset(f"irds:ir-lab-wise-2025/{dataset}").get_topics("title")
-    
+
     index = get_index(dataset, text_field_to_retrieve, output)
-    print(index)
-    #retriever = pt.terrier.Retriever(pt_index, wmodel=retrieval_model)
-
-    #tag = f"pyterrier-{retrieval_model}-on-{index}"
-    #target_dir = output / dataset / tag
-    #description = "This is a PyTerrier retriever using the retrieval model {retriever} retrieving on the {index} text representation of the documents. Everything is set to the defaults."
-
-    #with tracking(export_file_path=target_dir / "retrieval-metadata.yml", #export_format=ExportFormat.IR_METADATA, system_description=description, system_name=tag):
-    #    run = retriever(topics)
-
-    #pt.io.write_results(run, target_dir / "run.txt.gz")
+    run_retrieval(output, index, dataset, retrieval_model, text_field_to_retrieve)
+    
 
 if __name__ == '__main__':
     main()
